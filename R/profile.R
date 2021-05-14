9cbdd8779e297002d331ee558698279d215640d2
Sherry <huizezhangsh@gmail.com>
2021-05-14 16:34:08 +1000

    an outline for slice, profile and cube s3 class

---
+++ R/profile.R
@@ -0,0 +1,40 @@
+#' Profile the data cube
+#' @param dt the data cube to profile
+#'
+#' @return # here should be a customised proint of the profile
+#' @export
+profile_cube <- function(dt){
+
+  #first step is to detect the three axis
+  # produce a vector of axis (axis <- c(station, time, param))
+
+  map(axis, profile_single)
+
+  # then customise a print for the profile
+  # - id <station>
+  #   - name
+  #   - lat
+  #   - long
+  # - param <parameter>
+  # - time <time: interval>
+  #
+}
+
+
+profile_single <- function(dt, axis){
+
+  axis <- enquo(axis)
+  distinct_class <- dt %>% distinct(!!axis) %>% nrow()
+
+  count_table <- dt %>%
+    group_by(!!axis) %>%
+    summarise_all(n_distinct) %>%
+    ungroup() %>%
+    summarise_if(is.numeric, sum)
+
+  var_non_varying <- names(count_table)[count_table==distinct_class]
+  var_varying <- names(count_table)[count_table!=distinct_class]
+
+  tibble(!!axis := var_non_varying)
+}
+
---
+++ R/slice.R
@@ -0,0 +1,32 @@
+#' Slice the data cube into lower dimension
+#' @param dt the raw dataframe to slice, in long form - does it has to be long form?
+#' @param key the dimension to slice
+#'
+#' @return a nested tibble/ tsibble - think through here
+#' @example
+#' raw %>% slice_by(station)
+#' raw %>% slice_by(param)
+#' raw %>% slice_by(time)
+#' @export
+slice_by <- function(dt, key){
+  # implement multiple keys
+  key <- enquo(key)
+
+  distinct_class <- dt %>% dplyr::distinct(!!key) %>% nrow()
+
+  count_table <- dt %>%
+    dplyr::group_by(!!key) %>%
+    # summarise_all is superseded - change to use across
+    dplyr::summarise_all(n_distinct) %>%
+    dplyr::ungroup() %>%
+    # summarise_if is superseded - change to use across
+    dplyr::summarise_if(is.numeric, sum)
+
+  var_non_varying <- names(count_table)[count_table==distinct_class]
+  var_varying <- names(count_table)[count_table!=distinct_class]
+
+  dt %>%
+    tidyr::nest(-c(!!key, var_non_varying))
+}
+
+
