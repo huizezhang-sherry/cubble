0492f7e47fe84cce8cdfbecd296369939fe63d86
Sherry <huizezhangsh@gmail.com>
2021-06-16 21:24:18 +1000

    different treatement for wide/long form

--- R/data-masking.R
+++ R/data-masking.R
@@ -1,3 +1,9 @@
+#' Data structure
+#' @examples
+#' global <- global(oz_climate, station)
+#' zoom <- global %>% zoom(data)
+#' @export
+#' @rdname data-structure
 global <- function(data, key){
 
   key <- enquo(key)
@@ -9,24 +15,33 @@ global <- function(data, key){
   list_col <- data %>%
     tidyr::nest(!!!nest_var) %>%
     dplyr::rowwise()
-  meta_data <- as_tibble(data)[non_varying_var]
+  meta_data <- list_col[non_varying_var]
 
-  cubble_df(list_col, group_vars = as_name(key), meta_data = meta_data)
+
+  cubble_df(list_col, group_vars = as_name(key), meta_data = meta_data, format = "wide")
 
 }
 
-cubble_df <- function(data, group_vars, meta_data){
-  group_data <- as_tibble(data)[group_vars]
-  new_cubble_df(data, group_data, meta_data)
+cubble_df <- function(data, group_vars, meta_data, format){
+
+  new_cubble_df(data, group_vars, meta_data, format = format)
 }
 
 
-new_cubble_df <- function(data, group_data, meta_data){
-  nrow <- nrow(data)
+new_cubble_df <- function(data, group_vars, meta_data, format){
 
-  group_data <- new_tibble(dplyr:::dplyr_vec_data(group_data), nrow = nrow)
-  group_data$.rows <- new_list_of(as.list(seq_len(nrow)), ptype = integer())
-  new_tibble(data, groups = group_data, meta = meta_data, class = "cubble_df")
+  if (format == "wide"){
+    nrow <- nrow(data)
+    group_data <- as_tibble(data)[group_vars]
+    group_data <- new_tibble(dplyr:::dplyr_vec_data(group_data), nrow = nrow)
+    group_data$.rows <- new_list_of(as.list(seq_len(nrow)), ptype = integer())
+  } else if (format == "long"){
+    nrow <- nrow(meta_data)
+    group_data <- dplyr:::compute_groups(data, as_name(group_vars))
+  }
+
+
+  new_tibble(data, groups = group_data, meta = meta_data, format = format, class = "cubble_df")
 }
 
 find_non_varying_var <- function(data, key){
@@ -42,6 +57,8 @@ find_non_varying_var <- function(data, key){
 
 }
 
+#' @export
+#' @rdname data-structure
 zoom <- function(dt, col){
 
   # check from a global_df
@@ -51,14 +68,25 @@ zoom <- function(dt, col){
     abort("The column to zoom need to be a list-column")
   }
 
-  group_var <- sym(names(a %@% groups)[1])
-  meta_data <- a %@% meta
+  group_var <- sym(names(dt %@% groups)[1])
+  meta_data <- dt %@% meta
 
   out <- dt %>%
     dplyr::select(group_var, !!col) %>%
     tidyr::unnest(!!col)
 
-  cubble_df(out, group_vars = group_var, meta_data = meta_data)
+  nrow <- nrow(dt)
+
+  cubble_df(out, group_vars = group_var, meta_data = meta_data, format = "long")
 }
 
 
+dplyr_col_modify.cubble_df <- function(data, cols){
+
+
+}
+
+
+dplyr_reconstruct.cubble_df <- function(data, template){
+
+}
