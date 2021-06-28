2bd676a8dce1e89dc04a053396852e48379ab447
Sherry <huizezhangsh@gmail.com>
2021-06-28 13:52:35 +1000

    fix generated messages

--- R/missing.R
+++ R/missing.R
@@ -12,17 +12,17 @@ add_missing_prct <- function(data, vars){
   # different scenarios for long/ list-column form
   # now assume list-column form
 
-  var_names <- data %>% zoom(data) %>% colnames()
+  var_names <- data %>% zoom() %>% names()
 
   if (!all(vars %in% var_names)){
     bad_vars <- var[which(!vars %in% var_names )]
-    abort(glue::glue("Variable {bad_vars} are not presented in the long form"))
+    abort(glue::glue("Variable not presented in the long form: {bad_vars}"))
   }
 
 
-  calls <- map(vars,  ~quo(sum(is.na(data[[.x]]))/length(data[[.x]])))
+  calls <- map(vars,  ~quo(sum(is.na(ts[[.x]]))/length(ts[[.x]])))
   names(calls) <- glue::glue("{vars}_missing")
-  data %>% mutate(missing = list(map(calls, ~eval(.x)))) %>% tidyr::unnest_auto(missing)
+  data %>% mutate(missing = list(map(calls, ~eval(.x)))) %>% tidyr::unnest_wider(missing)
 
 }
 
@@ -39,7 +39,7 @@ add_missing_dscrb <- function(data, cutoff = c(0, 0.95)){
   calls <- map(vars, ~quo(ifelse(!!.x >= cutoff_upper, "almost all missing",
                                  ifelse(!!.x <= cutoff_lower, "almost no missing", "some missing"))))
 
-  vars_name <- stringr::str_extract(vars, "[^_]+")
+  vars_name <- map_chr(vars, ~as_name(.x) %>% stringr::str_extract(".*?(?=\\_)"))
   names(calls) <- glue::glue("{vars_name}_dscrb")
 
   dscrb <- purrr::map_dfr(calls, ~eval_tidy(.x, data = data))
