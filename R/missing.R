cef60f9324ccf92d58b810a2b7766a121af13152
Sherry <huizezhangsh@gmail.com>
2021-06-28 16:16:35 +1000

    documentation

--- R/missing.R
+++ R/missing.R
@@ -1,11 +1,18 @@
+#' Compute statistics for variable missingness
+#'
+#' @param data a cubble object
+#' @param vars the variable to compute missingness
+#' @param cutoff the lower and upper threshold value, below or above which the variable
+#'   will be described as "almost no missing" and "almost all missing". ("some missing" for
+#'   if in between). Only used in \code{add_missing_dscrb()}
 #' @examples
 #'
 #' \dontrun{
 #' out <- oz_global2 %>% add_missing_prct(vars = c("prcp", "tmin", "tmax"))
 #' out2 <- out %>% add_missing_dscrb()
 #' }
+#' @rdname missing
 add_missing_prct <- function(data, vars){
-
 
   test_cubble(data)
 
@@ -22,13 +29,15 @@ add_missing_prct <- function(data, vars){
 
   calls <- map(vars,  ~quo(sum(is.na(ts[[.x]]))/length(ts[[.x]])))
   names(calls) <- glue::glue("{vars}_missing")
-  data %>% mutate(missing = list(map(calls, ~eval(.x)))) %>% tidyr::unnest_wider(missing)
+  out <- data %>% mutate(missing = list(map(calls, ~eval(.x)))) %>% tidyr::unnest_wider(missing)
 
+  cubble_df(out, group = group_vars(data), meta_data = meta(data), form = determine_form(data))
 }
 
-
+#' @rdname missing
+#' @importFrom purrr map_dfr
 add_missing_dscrb <- function(data, cutoff = c(0, 0.95)){
-  #test_cubble(data)
+  test_cubble(data)
 
   all_names <- names(data)
   vars <- syms(all_names[stringr::str_detect(all_names, "missing")])
@@ -43,7 +52,8 @@ add_missing_dscrb <- function(data, cutoff = c(0, 0.95)){
   names(calls) <- glue::glue("{vars_name}_dscrb")
 
   dscrb <- purrr::map_dfr(calls, ~eval_tidy(.x, data = data))
-  data %>% dplyr::bind_cols(dscrb)
+  out <- tibble::as_tibble(data) %>% dplyr::bind_cols(dscrb)
 
+  cubble_df(out, group = group_vars(data), meta_data = meta(data), form = determine_form(data))
 
 }
