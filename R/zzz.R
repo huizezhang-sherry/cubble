c4a31194513f5428414f9ba00b84248e2ef070b4
Sherry <huizezhangsh@gmail.com>
2021-07-15 22:28:12 +1000

    update on an example

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
-onLoad <- function(...) {
+.onLoad <- function(...) {
   s3_register("dplyr::dplyr_col_modify", "cubble_df")
   s3_register("dplyr::dplyr_row_slice", "cubble_df")
   s3_register("dplyr::dplyr_reconstruct", "cubble_df")
