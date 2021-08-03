e7e6dfcf1ab0593845115149963606d2f0ed5092
Sherry <huizezhangsh@gmail.com>
2021-08-03 16:38:39 +1000

    simplify the selection in migrate

--- R/migrate.R
+++ R/migrate.R
@@ -9,26 +9,16 @@
 #' # Currently, switch back to global model will remove those migrated variables
 #' out %>% global()
 #' }
-#' @importFrom dplyr left_join
 #' @export
 migrate <- function(data, ...){
 
-  vars <- enquos(...)
   test_cubble(data)
 
   if (form(data) != "long"){
     abort("data needs to be in long form, convert using `zoom()`")
   }
 
-  out <- map_lgl(vars, ~as_name(.x) %in% names(meta(data)))
-
-  if (!all(out)){
-    abort("only variables in the metadata can be migrated into the long form. See metadata with `meta()`")
-  }
-
-  key <- group_vars(data)
-  key_to_join <- key[key %in% names(meta(data))]
-  to_join <- meta(data) %>% dplyr::select(key_to_join, !!!vars)
-  data %>% left_join(to_join, by = key_to_join)
+  to_join <- meta(data) %>% select(group_vars(data), ...)
+  data %>% left_join(to_join)
 
 }
