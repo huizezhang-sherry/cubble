7a35655589d5749e51cb67ffb73be22cb80e6cb5
Sherry <huizezhangsh@gmail.com>
2021-08-13 19:16:33 +1000

    more renaming and misc

--- R/condition.R
+++ R/condition.R
@@ -1,3 +1,13 @@
 test_cubble <- function(data) {
   if (!is_cubble(data)) abort("data supplied needs to be a cubble object!")
 }
+
+test_long <- function(data){
+  test_cubble(data)
+  if (form(data) != "long") abort("data is not in the long form")
+}
+
+test_nested <- function(data){
+  test_cubble(data)
+  if (form(data) != "nested") abort("data is not in the nested form")
+}
---
+++ R/stretch.R
@@ -0,0 +1,53 @@
+#' Switch a cubble object into a long form
+#' @param data a cubble object
+#' @param key the spatio identifier. Key can be automatically detected for a cubble object
+#' @details
+#' `stretch()` switch a cubble object into the long form where the combination of group identifier
+#' and timestamp defines a row. The long form cubble is always of class `cubble_df` and `grouped_df`.
+#'
+#' @examples
+#' climate_flat %>% tamp(station) %>% stretch()
+#'
+#' @export
+#' @seealso Other cubble verbs include \code{\link{tamp}} and \code{\link{migrate}}
+stretch <- function(data, key) {
+  test_cubble(data)
+  UseMethod("stretch")
+}
+
+#' @export
+stretch.cubble_df <- function(data, key){
+
+  col <- sym(names(data)[map_chr(data, class) == "list"])
+
+  if (!is_list(eval_tidy(col, data))) {
+    abort("The column to stretch need to be a list-column")
+  }
+
+  key <- enquo(key)
+
+  if (quo_is_missing(key)){
+    key <- quo(!!group_vars(data))
+  } else{
+    key <- quo_get_expr(key)
+  }
+
+  leaves_data <- tibble::as_tibble(data[, find_non_varying_var(data, !!key)])
+
+
+  list_col <- data %>% dplyr::pull(!!col)
+
+  if ("tbl_ts" %in% class(list_col[[1]])){
+    data$ts <- map(data$ts, tibble::as_tibble)
+    out <- data[vec_c(as_name(key), as_name(col))]
+    out <- out %>%
+      tidyr::unnest(!!col) %>%
+      tsibble::as_tsibble(key = !!key)
+  } else{
+    out <- data %>%
+      dplyr::select(!!key, !!col) %>%
+      tidyr::unnest(!!col)
+  }
+
+  cubble_df(out, group = as_name(key), leaves = leaves_data, form = "long")
+}
