d96a96f081b213a076bd47eed99d67cc1cbd56a3
Sherry <huizezhangsh@gmail.com>
2021-08-19 14:32:58 +1000

    new function: initiate a ggplot2 object with map

---
+++ R/map.R
@@ -0,0 +1,25 @@
+#' A quick plot of maps
+#'
+#' A quick initiate of a ggplot2 object for map without typing
+#' `data %>% ggplot2::ggplot() + ggplot2::geom_sf(ggplot2::aes(geometry = geometry))`
+#' everytime to create a map.
+#'
+#' @param map_data the dataset contains the map to be plotted
+#' @param ... other arguments to be passed into `geom_sf()`
+#' @examples
+#' library(rmapshaper)
+#' state_map <- ms_simplify(ozmaps::abs_ste, keep = 2e-3)
+#' plot_map(state_map)
+#' @export
+plot_map <- function(map_data, ...){
+
+  if ("geometry" %in% names(map_data)){
+    ggplot2::ggplot() +
+      ggplot2::geom_sf(data = map_data, ggplot2::aes(geometry = .data$geometry), ...) +
+      ggplot2::coord_sf() +
+      ggplot2::theme_void()
+  } else{
+    abort("`map_data` needs to have a column called `geometry`")
+  }
+
+}
