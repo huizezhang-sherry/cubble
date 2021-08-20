b150818798ef3891478373472ee5397dbaac7072
Sherry <huizezhangsh@gmail.com>
2021-08-20 14:39:53 +1000

    some doc + clean up

--- R/map.R
+++ R/map.R
@@ -1,13 +1,13 @@
 #' A quick plot of maps
 #'
-#' A quick initiate of a ggplot2 object for map without typing
-#' `data %>% ggplot2::ggplot() + ggplot2::geom_sf(ggplot2::aes(geometry = geometry))`
-#' everytime to create a map.
+#' A quick initiate of a ggplot2 object for map with some default aesthetic setting
+#' to save some typing for create a map.
 #'
 #' @param map_data the dataset contains the map to be plotted
 #' @param ... other arguments to be passed into `geom_sf()`
 #' @examples
 #' library(rmapshaper)
+#' library(ggplot2)
 #' state_map <- ms_simplify(ozmaps::abs_ste, keep = 2e-3)
 #' plot_map(state_map)
 #' plot_map(state_map) +
@@ -16,7 +16,9 @@
 plot_map <- function(map_data, ...){
   if ("geometry" %in% names(map_data)){
     ggplot2::ggplot()  +
-      ggplot2::geom_sf(data = map_data, ggplot2::aes(geometry = .data$geometry), ...) +
+      ggplot2::geom_sf(data = map_data,
+                       ggplot2::aes(geometry = geometry),
+                       color = "grey80", alpha = 0.4, linetype = 3, ...) +
       ggplot2::coord_sf() +
       ggplot2::theme_void()
   } else{
