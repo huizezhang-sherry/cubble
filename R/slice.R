40d88806feff780d799cf42f07f6a109683e5834
huizezhangh-sherry <huizezhangsh@gmail.com>
2021-09-09 14:12:00 +1000

    styling

--- R/slice.R
+++ R/slice.R
@@ -64,19 +64,21 @@ slice_sample.cubble_df <- slice_factory("slice_sample")
 #' @export
 slice_nearby <- function(data, ..., buffer = 1, n = 5){
 
-  if (form(data) != "nested"){
+  if (form(data) != "nested") {
     abort("slicing should be performed in the nested form on the grouping variable")
   }
 
   dots <- enquos(...)
 
-  idx <- map(dots, ~eval_tidy(.x, data))
+  idx <- map(dots, ~ eval_tidy(.x, data))
   target <- vec_slice(data, Reduce("&", idx))
   target <- dplyr_reconstruct(target, data)
 
-  if (nrow(target) == 0){
-    abort("No candidate fulfill the condition(s),
-          try `data %>% filter(...) on the condition to diagnose`")
+  if (nrow(target) == 0) {
+    abort(
+      "No candidate fulfill the condition(s),
+          try `data %>% filter(...) on the condition to diagnose`"
+    )
   }
 
   lat_min <-  min(target$lat) - buffer
@@ -87,19 +89,24 @@ slice_nearby <- function(data, ..., buffer = 1, n = 5){
   gvar <- group_vars(data)
   target_var <- groups(target) %>% dplyr::pull(!!group_vars(data))
 
-  cand <- data %>% filter(dplyr::between(.data$lat, lat_min, lat_max),
-                          dplyr::between(.data$long, long_min, long_max),
-                                 !(!!sym(gvar) %in% target_var))
+  cand <-
+    data %>% filter(
+      dplyr::between(.data$lat, lat_min, lat_max),
+      dplyr::between(.data$long, long_min, long_max),
+      !(!!sym(gvar) %in% target_var)
+    )
 
   inform(glue::glue("The bounding box gives {nrow(cand)} candidates"))
 
-  if (n > nrow(cand)){
+  if (n > nrow(cand)) {
     inform("Number of element to sample is larger than the candidate.
            All candidates are selected")
   }
 
-  out <- rbind(target %>% mutate(type = "selected"),
-        cand %>% slice_sample(n = n) %>% mutate(type = "sampled"))
+  out <- rbind(
+    target %>% mutate(type = "selected"),
+    cand %>% slice_sample(n = n) %>% mutate(type = "sampled")
+  )
   out <- dplyr_reconstruct(out, data)
   attr(out, "bbox") <- c(long_min, long_max, lat_min, lat_max)
 
@@ -118,30 +125,31 @@ slice_nearby <- function(data, ..., buffer = 1, n = 5){
 #'                map = ozmaps::abs_ste %>% dplyr::filter(NAME == "Victoria"))
 #' @export
 #' @rdname nearby
-view_nearby <- function(data, origin, map = NULL){
-
-
-  if (!"type" %in% colnames(data)){
+view_nearby <- function(data, origin, map = NULL) {
+  if (!"type" %in% colnames(data)) {
     abort("Data needs to have the `type` column outputted from `slice_nearby`!")
   }
 
-  if (is.null(map)){
+  if (is.null(map)) {
     map <- rmapshaper::ms_simplify(ozmaps::abs_ste, keep = 2e-3)
   }
 
   bbx <- attr(data, "bbox")
-  all <- origin %>% filter(dplyr::between(.data$lat, bbx[3], bbx[4]),
-                           dplyr::between(.data$long, bbx[[1]], bbx[[2]]))
+  all <- origin %>%
+    filter(dplyr::between(.data$lat, bbx[3], bbx[4]),
+           dplyr::between(.data$long, bbx[[1]], bbx[[2]]))
 
   plot_map(map_data = map) +
-    ggplot2::geom_point(data = all, ggplot2::aes(x = .data$long, y = .data$lat), color = "grey") +
-    ggplot2::geom_point(data = data, ggplot2::aes(x = .data$long, y = .data$lat, color = .data$type)) +
-    ggplot2::geom_rect(data = data, ggplot2::aes(xmin = bbx[1],
-                                                 xmax = bbx[2],
-                                                 ymin = bbx[3],
-                                                 ymax = bbx[4]),
-                       linetype = "dashed",
-                       fill = "transparent", color = "black") +
+    ggplot2::geom_point(data = all,
+                        ggplot2::aes(x = .data$long, y = .data$lat),
+                        color = "grey") +
+    ggplot2::geom_point(data = data,
+                        ggplot2::aes(x = .data$long, y = .data$lat,
+                                     color = .data$type)) +
+    ggplot2::geom_rect(data = data,
+                       ggplot2::aes(xmin = bbx[1], xmax = bbx[2],
+                                    ymin = bbx[3], ymax = bbx[4]),
+                       linetype = "dashed", fill = "transparent", color = "black") +
     ggplot2::scale_color_brewer(palette = "Dark2")
 
 }
