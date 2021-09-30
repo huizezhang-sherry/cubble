6216fe22a294bc7cbe2d2a19a6302fae494cad42
Sherry <huize.zhang@monash.edu>
2021-09-30 22:48:24 +1000

    NEW FUNCTIONS strip_rowwise, switch_key, rename_key

--- R/rowwise.R
+++ R/rowwise.R
@@ -1,23 +1,79 @@
+#' Remove the rowwise grouping of a cubble
+#'
+#' @param data a cubble object
+#' @examples
+#' library(dplyr)
+#' # row number is not properly added since each row is a separate group
+#' climate_small %>% mutate(.id = row_number())
+#'
+#' # proper id after removing the grouping structure
+#' climate_small %>% strip_rowwise() %>% mutate(.id = row_number())
+#'
 #' @export
 strip_rowwise <- function(data){
+
+  test_cubble(data)
   attr(data, "groups") <- attr(data, "groups")[NULL, ]
   attr(data, "class") <- attr(data, "class")[attr(data, "class") != "rowwise_df"]
 
   data
 }
 
+#' Switch to a different key of a cubble
+#' @param data a cubble
+#' @param new_key the new key
+#' @param back whether it is a switch back to the individual level
+#'
+#' @examples
+#' library(dplyr)
+#' library(ggplot2)
+#' library(weatherdata)
+#' # create a data with hierarchical structure:
+#' # id (station) is nested in country, country is nested in continent
+#' long <- world_sample %>%
+#'   filter(country != "Algeria") %>%
+#'   stretch() %>%
+#'   migrate(country, continent) %>%
+#'   mutate(tmax = tmax/10) %>%
+#'   filter(lubridate::year(date) == 2020)
+#'
+#' # switch the key from id to country to aggregate on country
+#' country_wise <- long %>%
+#'   switch_key(country) %>%
+#'   group_by(date) %>%
+#'   summarise(tmax = mean(tmax, na.rm = TRUE))
+#'
+#' # plot the aggregated maximum temperature for each country
+#' country_wise %>%
+#'   ggplot(aes(x = date, y = tmax, group = country)) +
+#'   geom_line() +
+#'   facet_wrap(vars(country)) +
+#'   scale_x_date(date_labels = "%b")
 #' @export
-switch_key <- function(data, new_key){
+switch_key <- function(data, new_key, back = FALSE){
   new_key <- enquo(new_key)
   test_cubble(data)
-  out <- data %>% as_tibble()
-  new_cubble(out,
+
+  if (back){
+    leaves <- new_leaves(data %>% unnest_cubble(tsibble_key = !!new_key), !!new_key)
+  } else{
+    leaves <- new_leaves(data %>% unnest_cubble(), !!new_key)
+  }
+
+  new_cubble(data,
              key = as_name(new_key), index = index(data), coords = coords(data),
-             leaves = new_leaves(out %>% unnest(), !!new_key), form = determine_form(data))
+             leaves = leaves, form = determine_form(data))
 }
 
+#' Rename the key variable
+#' @param data a cubble
+#' @param ... argument passed to \code{rename}: NEW = OLD
+#'
+#' @examples
+#' climate_small %>% rename_key(id = station)
 #' @export
 rename_key <- function(data, ...){
+  test_cubble(data)
   out <- data %>% as_tibble() %>% rename(...)
 
   new_cubble(out,
