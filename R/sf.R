cae02e0a62d910594f3d7d86fdba1a433377c775
Sherry Zhang <sherryzhang@dyn-49-127-24-11.its.monash.edu.au>
2021-11-17 13:21:01 +1100

    allow coords to be an sfc

---
+++ R/sf.R
@@ -0,0 +1,9 @@
+#' @importFrom sf st_coordinates
+convert_sfc_to_ll <- function(data, coords){
+  dt <- data %>%
+    dplyr::mutate(ll = list(tibble::as_tibble(sf::st_coordinates(geom)))) %>%
+    dplyr::select(-geom)
+  coords <- c("X", "Y")
+
+  list(data = dt, coords = coords)
+}
