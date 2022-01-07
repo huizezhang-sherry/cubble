6ee8d83e7671da1ea1cb81634f9c8773711481a9
Sherry Zhang <huizezhangsh@gmail.com>
2022-01-07 23:17:12 +1100

    seperate geom_glyph_line and geom_glyph_box functions for reference line and box

--- R/glyph-map.R
+++ R/glyph-map.R
@@ -11,18 +11,17 @@
 #'  the \code{\link[ggplot2]{resolution}} of the data. Specify the width
 #'  absolutely by supplying a numeric vector of length 1, or relative to the
 #'  resolution of the data by using \code{\link[ggplot2]{rel}}.
-#' @param ref_line,ref_box Wether to include the horizontal middle reference
-#' line and the reference bounding box.
 #' @export
 #' @examples
 #' library(ggplot2)
 #' library(GGally)
 #' # basic glyph map with reference line and box---------------
-#' ggplot() +
-#'   geom_glyph(data = GGally::nasa,
-#'              aes(x_major = long, x_minor = day,
-#'                  y_major = lat, y_minor = surftemp),
-#'              ref_box = TRUE, ref_line = TRUE)+
+#' ggplot(data = GGally::nasa,
+#'        aes(x_major = long, x_minor = day,
+#'            y_major = lat, y_minor = surftemp)) +
+#'   geom_glyph_box() +
+#'   geom_glyph_line() +
+#'   geom_glyph() +
 #'   theme(aspect.ratio = 1)
 #'
 #' # with polar coordinate ---------------
@@ -46,7 +45,6 @@ geom_glyph <- function(mapping = NULL, data = NULL, stat = "identity",
                        x_minor = NULL, y_major = NULL, y_minor = NULL,
                        polar = FALSE, width = ggplot2::rel(1.5),
                        height = ggplot2::rel(1.5),
-                       ref_line = FALSE, ref_box = FALSE,
                        show.legend = NA,
                        inherit.aes = TRUE) {
   ggplot2::layer(
@@ -59,8 +57,6 @@ geom_glyph <- function(mapping = NULL, data = NULL, stat = "identity",
     inherit.aes = inherit.aes,
     params = list(
       polar = polar,
-      ref_line = ref_line,
-      ref_box = ref_box,
       width = width,
       height = height,
       ...
@@ -73,86 +69,117 @@ GeomGlyph <- ggplot2::ggproto(
   ggplot2::Geom,
   setup_data = function(data, params) {
 
-    if (length(unique(data$group)) == 1){
-      data$group <- interaction(data$x_major, data$y_major, drop = TRUE)
-    }
-
-    data$polar <- params$polar
-    data$width <- ggplot2::resolution(data$x_major, zero = FALSE) * params$width
-    data$height <- ggplot2::resolution(data$y_major, zero = FALSE) * params$height
-
-    if (params$polar) {
-      theta <- 2 * pi * rescale01(data$x_minor)
-      r <- rescale01(data$y_minor)
-
-      data$x <- data$x_major + params$width  / 2 * r * sin(theta)
-      data$y <- data$y_major + params$height / 2 * r * cos(theta)
-      data <- data[order(data$x_major, data$x_minor), ]
-
-    } else {
-      data$x <- data$x_major + rescale11(data$x_minor) * params$width / 2
-      data$y <- data$y_major + rescale11(data$y_minor) * params$height / 2
-
-    }
-
-    data
+    glyph_data_setup(data, params)
   },
 
   draw_panel = function(data,  panel_params, coord, ...) {
-    grid::gList(if (any(data$ref_box)) {
-      ref_box <- calc_ref_box(data)
-      ggplot2:::GeomRect$draw_panel(ref_box, panel_params, coord, ...)
-    },
-    if (any(data$ref_line)) {
-      ref_line <- calc_ref_line(data)
-      ggplot2:::GeomPath$draw_panel(ref_line, panel_params, coord, ...)
-    },
-    ggplot2:::GeomPath$draw_panel(data, panel_params, coord, ...))
+    ggplot2:::GeomPath$draw_panel(data, panel_params, coord, ...)
   },
 
 
   required_aes = c("x_minor", "x_major", "y_minor", "y_major"),
   default_aes = ggplot2::aes(
-    colour = "black", size = 0.5, linetype = 1, alpha = 1,
+    colour = "black", size = 0.5, linetype = "solid", alpha = 1,
     polar = FALSE,
     width = ggplot2::rel(1.5),
-    height = ggplot2::rel(1.5),
-    ref_line = FALSE,
-    ref_box = FALSE
+    height = ggplot2::rel(1.5)
   )
 )
 
-
 #' @export
-#' @rdname geom_glyph
-stat_glyph <- function(mapping = NULL, data = NULL, geom = "identity",
-                       position = "identity", ...,
-                       x_major = NULL, x_minor = NULL,
-                       y_major = NULL, y_minor = NULL, polar = FALSE,
-                       width = ggplot2::rel(1.5),
-                       height = ggplot2::rel(1.5),
-                       show.legend = NA,
-                       inherit.aes = TRUE) {
+geom_glyph_line <- function(mapping = NULL, data = NULL, stat = "identity",
+                            position = "identity", ..., x_major = NULL,
+                            x_minor = NULL, y_major = NULL, y_minor = NULL,
+                            polar = FALSE, width = ggplot2::rel(1.5),
+                            height = ggplot2::rel(1.5),
+                            show.legend = NA,
+                            inherit.aes = TRUE) {
   ggplot2::layer(
     data = data,
     mapping = mapping,
-    stat = StatGlyph,
-    geom = geom,
+    stat = stat,
+    geom = GeomGlyphLine,
     position = position,
     show.legend = show.legend,
     inherit.aes = inherit.aes,
     params = list(
-      x_major = NULL,
-      x_minor = NULL,
-      y_major = NULL,
-      y_minor = NULL,
-      polar = FALSE,
+      polar = polar,
       width = width,
       height = height,
       ...
     )
   )
 }
+
+GeomGlyphLine <- ggplot2::ggproto(
+  "GeomGlyphLine",
+  ggplot2::Geom,
+  setup_data = function(data, params) {
+    data <- glyph_data_setup(data, params)
+    calc_ref_line(data, params)
+  },
+
+  draw_panel = function(data,  panel_params, coord, ...) {
+    ggplot2:::GeomPath$draw_panel(data, panel_params, coord, ...)
+  },
+
+
+  required_aes = c("x_minor", "x_major", "y_minor", "y_major"),
+  default_aes = ggplot2::aes(
+    colour = "grey", size = 1, linetype = "solid", alpha = 1,
+    polar = FALSE,
+    width = ggplot2::rel(1.5),
+    height = ggplot2::rel(1.5),
+  )
+)
+
+#' @export
+geom_glyph_box <- function(mapping = NULL, data = NULL, stat = "identity",
+                           position = "identity", ..., x_major = NULL,
+                           x_minor = NULL, y_major = NULL, y_minor = NULL,
+                           polar = FALSE, width = ggplot2::rel(1.5),
+                           height = ggplot2::rel(1.5),
+                           show.legend = NA,
+                           inherit.aes = TRUE) {
+  ggplot2::layer(
+    data = data,
+    mapping = mapping,
+    stat = stat,
+    geom = GeomGlyphBox,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(
+      polar = polar,
+      width = width,
+      height = height,
+      ...
+    )
+  )
+}
+
+GeomGlyphBox <- ggplot2::ggproto(
+  "GeomGlyphBox",
+  ggplot2::Geom,
+  setup_data = function(data, params) {
+    data <- glyph_data_setup(data, params)
+    calc_ref_box(data, params)
+  },
+
+  draw_panel = function(data,  panel_params, coord, ...) {
+    ggplot2:::GeomRect$draw_panel(data, panel_params, coord, ...)
+  },
+
+
+  required_aes = c("x_minor", "x_major", "y_minor", "y_major"),
+  default_aes = ggplot2::aes(
+    colour = "grey", size = 0.5, linetype = "solid", alpha = 1,
+    fill = "transparent",
+    polar = FALSE,
+    width = ggplot2::rel(1.5),
+    height = ggplot2::rel(1.5),
+  )
+)
 
 # Helpers ------------------------------------------
 rescale01 <- function(x, xlim=NULL) {
@@ -163,11 +190,38 @@ rescale01 <- function(x, xlim=NULL) {
   }
   (x - rng[1]) / (rng[2] - rng[1])
 }
+
 rescale11 <- function(x, xlim = NULL) 2 * rescale01(x, xlim) - 1
 
-calc_ref_line <- function(data){
-  ref_line <- data %>% dplyr::select(-c(x_minor, y_minor, x, y))
+glyph_data_setup <- function(data, params){
+  if (length(unique(data$group)) == 1){
+    data$group <- interaction(data$x_major, data$y_major, drop = TRUE)
+  }
 
+  data$polar <- params$polar
+  data$width <- ggplot2::resolution(data$x_major, zero = FALSE) * params$width
+  data$height <- ggplot2::resolution(data$y_major, zero = FALSE) * params$height
+
+  if (params$polar) {
+    theta <- 2 * pi * rescale01(data$x_minor)
+    r <- rescale01(data$y_minor)
+
+    data$x <- data$x_major + params$width  / 2 * r * sin(theta)
+    data$y <- data$y_major + params$height / 2 * r * cos(theta)
+    data <- data[order(data$x_major, data$x_minor), ]
+
+  } else {
+    data$x <- data$x_major + rescale11(data$x_minor) * params$width / 2
+    data$y <- data$y_major + rescale11(data$y_minor) * params$height / 2
+
+  }
+
+  data
+}
+
+
+calc_ref_line <- function(data, params){
+  ref_line <- data
 
   if (any(data$polar)) {
     theta <- seq(0, 2 * pi, length.out = 30)
@@ -178,31 +232,26 @@ calc_ref_line <- function(data){
     )
   } else{
     ref_line <- ref_line %>% transform(group = group,
-                                       x = x_major +  width / 2,
+                                       x = x_major + params$width / 2,
                                        y = y_major) %>%
       rbind(ref_line %>% transform(
         group = group,
-        x = x_major - width / 2,
+        x = x_major - params$width / 2,
         y = y_major
       )
       )
   }
 
-  ref_line %>% transform(colour = "grey",
-                         size = 1)
+  ref_line
 }
 
 
-calc_ref_box <- function(data){
-
+calc_ref_box <- function(data, params){
   ref_box <- data %>%
-    dplyr::select(-c(x_minor, y_minor, x, y)) %>%
-    dplyr::mutate(xmin = x_major - width / 2,
-                   xmax = x_major + width / 2,
-                   ymin = y_major - height / 2,
-                   ymax = y_major + height / 2,
-                  colour = "grey",
-                  fill = "transparent")
+    dplyr::mutate(xmin = x_major -  params$width / 2,
+                   xmax = x_major + params$width / 2,
+                   ymin = y_major - params$height / 2,
+                   ymax = y_major + params$height / 2)
   ref_box
 }
 
