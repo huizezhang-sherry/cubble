479afeb7c7989ba514d10f87515147557ecbff6c
Sherry Zhang <huizezhangsh@gmail.com>
2022-02-14 11:18:29 +1100

    misc

--- R/map.R
+++ R/map.R
@@ -22,11 +22,13 @@
 plot_map <- function(map_data, ...){
   if ("geometry" %in% names(map_data)){
     ggplot2::ggplot()  +
-      ggplot2::geom_sf(data = map_data,
-                       ggplot2::aes(geometry = .data$geometry),
-                       color = "grey80", alpha = 0.4, linetype = 3, ...) +
-      ggplot2::coord_sf() +
-      ggplot2::theme_void()
+      ggplot2::geom_sf(
+        data = map_data,
+        ggplot2::aes(geometry = .data$geometry),
+        fill = "transparent", color = "grey", linetype = "dotted", ...) +
+      ggplot2::theme_bw() +
+      ggplot2::theme(legend.position = "bottom") +
+      ggplot2::labs(x = "Longitude", y = "Latitude")
   } else{
     cli::cli_abort("{.fn map_data} needs to have a column called {.field geometry}")
   }
